7bef56a28d1406c90b7a7083074af37aad3b1ca8
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2023-01-13 15:58:15 -0500

    Initial commit

---
+++ src/main/java/com/jrp/pma/ProjectManagement23Application.java
@@ -0,0 +1,13 @@
+package com.jrp.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagement23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagement23Application.class, args);
+    }
+
+}
