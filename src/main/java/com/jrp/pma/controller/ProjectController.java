8adfbcb796e74b3802251d15c36798b00a9e1984
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2023-01-13 19:23:05 -0500

    Create Crud Repository for Project Entity

--- src/main/java/com/jrp/pma/controller/ProjectController.java
+++ src/main/java/com/jrp/pma/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.jrp.pma.controller;
 
+import com.jrp.pma.dao.ProjectRepository;
 import com.jrp.pma.entity.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+
+    final
+    ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
@@ -23,7 +31,9 @@ public class ProjectController {
 
     @PostMapping(value = "/save")
     public String createProject(Project project, Model model) {
-        // TODO: this should handle saving to the database...
-        return "";
+        projectRepository.save(project);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/projects/new";
     }
 }
---
+++ src/main/java/com/jrp/pma/dao/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.jrp.pma.dao;
+
+import com.jrp.pma.entity.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
