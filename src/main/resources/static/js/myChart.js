9f71a85a2e1d55148fb44259056513ce98beee51
Vladimir Apraksin (OSV) <vladimir.apraksin.osv@fedex.com>
2023-01-15 22:45:33 -0500

    Improving the Homepage

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,17 +1,21 @@
-const myPieChart = {
-    labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-    ],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-    }]
-};
+const ctx = document.getElementById('myPieChart');
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ['rgba(62, 149, 205, 1)', 'rgba(142, 94, 162, 1)', 'rgba(60, 186, 159, 1)'],
+            data: [5, 10, 5],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+})
